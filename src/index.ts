b37475f875eada46628e5bb1b4a55a71d424ed78
dtinth on MBP M1 <dtinth@spacet.me>
2022-09-08 20:57:35 +0700

    Add code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export {}
+import { useEffect, useState } from 'react'
+
+export interface PerformanceCoachOptions {
+  enabled?: boolean
+}
+
+export default function usePerformanceCoach(
+  options: PerformanceCoachOptions = {},
+) {
+  const enabled = !!(options.enabled ?? process.env.NODE_ENV !== 'production')
+  const [, setCount] = useState(0)
+  useEffect(() => {
+    if (!enabled) {
+      return
+    }
+    const interval = setInterval(() => {
+      setCount((count) => count + 1)
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [enabled])
+}
