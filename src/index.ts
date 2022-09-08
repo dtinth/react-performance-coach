f18b42e121591ad39d12fea48df42f9367f284b8
dtinth on MBP M1 <dtinth@spacet.me>
2022-09-09 00:09:38 +0700

    Add TSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
 
 /**
+ * Options for passing to the {@link usePerformanceCoach} hook.
  * @public
  */
 export interface PerformanceCoachOptions {
+  /**
+   * Whether to enable the hook. If false, the hook will not do anything.
+   * By default, this is true when not in production.
+   */
   enabled?: boolean
 }
 
 /**
+ * Makes a React component re-render every 1 second. Useful for uncovering performance issues.
  * @public
  */
 export default function usePerformanceCoach(
